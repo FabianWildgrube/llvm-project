b80334e568dfa397ed65d4122aa625600a3af04b
Fabian <fabian.wildgrube@amd.com>
2026-08-19 11:21:15 +0200

    Actually use the riscw backend when compiling with llc

--- test-riscw/compile-with-riscw.sh
+++ test-riscw/compile-with-riscw.sh
@@ -1,2 +1,2 @@
-../build/bin/clang -O2 -emit-llvm -target riscw-elf -c test.c -o testMine.bc
-../build/bin/llc -march=riscv64 -O2 -filetype=asm testMine.bc -o test.S
+../build/bin/clang -emit-llvm -target riscw-elf -c test.c -o testMine.bc
+../build/bin/llc -march=riscw -debug-only=riscw-isel -filetype=asm testMine.bc -o test.S
